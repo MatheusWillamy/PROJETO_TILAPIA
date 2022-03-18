455684f70536eb11d8adb1cc45dbf00f48df4392
MatheusWillamy <matheus2011willamy@hotmail.com.br>
2022-03-18 14:19:02 -0300

    Correção_duplicado

--- _linkers/serial_guia.js
+++ _linkers/serial_guia.js
@@ -1,16 +1,6 @@
-<<<<<<< HEAD
-
-
 var valor1 = document.getElementById("valor_1")
 valor1.addEventListener('click', serial_analise())
 
 console.log(message)
-=======
 
-
-var valor1 = document.getElementById("valor_1")
-valor1.addEventListener('click', serial_analise())
-
-console.log(message)
->>>>>>> 69e1d03098fb3a53cdafaec9f0c6c83312d9a579
 valor1.innerText=message
