12f70da31e3d92cd1d7b6f16aa90f40e57e54e46
MatheusWillamy <matheus2011willamy@hotmail.com.br>
2022-03-18 14:15:28 -0300

    correção index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const { app, BrowserWindow} = require('electron')
 
 let mainWindow
@@ -16,25 +15,4 @@ app.on('ready', () =>{
 
 
 
-
-=======
-const { app, BrowserWindow} = require('electron')
-
-let mainWindow
-
-
-app.on('ready', () =>{
-
-    mainWindow = new BrowserWindow({
-        width: 1280,
-        heigth:720
-
-    })
-
-    mainWindow.loadURL(`file://${__dirname}/_views/index.html`)
-
-
-
-
->>>>>>> 69e1d03098fb3a53cdafaec9f0c6c83312d9a579
 })
